6c67e9c6c93e6b84708b4f99e29b5d7cde1a4574
Ra-ba-b <58338771+Ra-ba-b@users.noreply.github.com>
2022-05-15 20:12:08 +0200

    Update parta.m

--- parta.m
+++ parta.m
@@ -3,6 +3,7 @@
 %Givens (total number of channels is S, the frequency in MHz is fMHz, BSh is the Base Station height in meters,
 % MSh is the Mobile Station height in meters, Rxmin is the MS sensitivity in dBm, traffic is the traffic intensity
 %per user in Erlang,and n is the path loss exponent)
+
 S = 340;
 fMHz = 900;
 BSh = 20;
@@ -12,26 +13,36 @@ trafficUser = 0.025;
 n = 4;
 
 %inputs to get from the user
+
 GOS = input('Enter the GOS ');
 cityArea = input('Enter the city area (in square kilometers) ');
 userDensity = input('Enter the user denisty (number of users in the given city area in users per square kilometer) ');
 SIRmin = input('Enter the minimum SIR in dB');
 sectorMethod = input('Enter the sectorization method (0 for omnidirectional, 1 for 60 degrees sectoring, and 2 for 120 degrees sectoring) ');
 
+%Calculations
+
+%converting SIR from dB to ratio
+
 SIRratio = 10^(SIRmin*0.1);
 
+%calculating the reuse distance according to the sectorization method used
 
 if sectorMethod == 0
-	reuseRatio = (6*SIRmin)^(0.25)+1
+	reuseDist = (6*SIRmin)^(0.25)+1;
 	sectorsNumber == 1;
 elseif sectorMethod == 1
-	reuseRatio = (SIRmin)^(0.25)+1
+	reuseDist = (SIRmin)^(0.25)+1;
 	sectorsNumber = 6;
 elseif sectorMethod == 2
-	reuseRatio = (2*SIRmin)^(0.25)+1
+	reuseDist = (2*SIRmin)^(0.25)+1;
 	sectorsNumber = 3;
 else errordlg('Please enter 0 for omnidirectional, 1 for 60 degrees sectoring, and 2 for 120 degrees sectoring','Error')
 end
+
+%calculating N (cluster size)
+
+N = reuseDist^2 / 3;
 	
 
 
