76e50a29e1de2e71d081b236c85a894ac8b64d11
Ra-ba-b <58338771+Ra-ba-b@users.noreply.github.com>
2022-05-15 20:44:18 +0200

    Update and rename parta.m to partA.m

--- partA.m
+++ partA.m
@@ -26,36 +26,16 @@ sectorMethod = input('Enter the sectorization method (0 for omnidirectional, 1 f
 
 SIRratio = 10^(SIRmin*0.1);
 
-%calculating the reuse distance according to the sectorization method used
+%calculating the number of interfering signals according to the sectorization method used
 
 if sectorMethod == 0
-	reuseDist = (6*SIRmin)^(0.25)+1;
-	sectorsNumber == 1;
+	interSig = 6;
 elseif sectorMethod == 1
-	reuseDist = (SIRmin)^(0.25)+1;
-	sectorsNumber = 6;
+	interSig = 1;
 elseif sectorMethod == 2
-	reuseDist = (2*SIRmin)^(0.25)+1;
-	sectorsNumber = 3;
+	interSig = 2;
 else errordlg('Please enter 0 for omnidirectional, 1 for 60 degrees sectoring, and 2 for 120 degrees sectoring','Error')
 end
-
-%generate an array B of frequency reuse factor possible values
-A=zeros(1,c);
-c=0;
-for i = 0:10
-    for k = 1:10
-       m = (i^2)+(i*k)+(k^2);
-       c=c+1;
-       A(1, c) = [m];
-       B=sort(unique(A));
-    end
-end
-
-%calculate cluster size N from the given SIRmin(dB)
-N_more_or_eq=(1/3)*(i*10^(SIRmin/(n*10)))^2; 
-N = B( find ( B > N_more_or_eq, 1));
-	
 
 
 %total traffic intensity
