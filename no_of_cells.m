c5e06c7643ce521fa21e610f77204545663322d1
Sohila Noureldin <53787050+sohila99@users.noreply.github.com>
2022-05-16 18:12:50 +0200

    Update no_of_cells.m

--- no_of_cells.m
+++ no_of_cells.m
@@ -1,2 +1,2 @@
-traffic_intensity_total = traffic_intensity_per_user * user density * city_area;
+traffic_intensity_total = traffic_intensity_per_user * userDensity * cityArea;
 no_of_cells = traffic_intensity_total/traffic_intensity_per_cell; 
