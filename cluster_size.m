1ff9a88f41cacf48eef62f7a9e14013a97a4f99c
Sohila Noureldin <53787050+sohila99@users.noreply.github.com>
2022-05-16 13:18:13 +0200

    Update cluster_size.m

--- cluster_size.m
+++ cluster_size.m
@@ -1,19 +1,17 @@
 %part A.1
 
 %generate an array B of frequency reuse factor possible values
-A=zeros(1,c);
+X=zeros(1,100);
 c=0;
-for i = 0:10
+for j = 0:10
     for k = 1:10
-       m = (i^2)+(i*k)+(k^2);
+       m = (j^2)+(j*k)+(k^2);
        c=c+1;
-       A(1, c) = [m];
-       B=sort(unique(A));
+       X(1, c) = [m];
+       B=sort(unique(X));
     end
 end
 
-SIRmin=20;
-n=4; %path loss exponent
 %calculate cluster size N from the given SIR(dB)
 N_more_or_eq=(1/3)*(i*10^(SIRmin/10))^(2/n); 
 N = B( find ( B > N_more_or_eq, 1));
