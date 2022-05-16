64eb8727640c866e276f89e8e674834f2231d583
sama232000 <85187053+sama232000@users.noreply.github.com>
2022-05-16 23:15:26 +0200

    Update calculate_radius.m

--- calculate_radius.m
+++ calculate_radius.m
@@ -1,6 +1,4 @@
-function [cell_radius]=calculate_radius(no_of_cells)
-
-city_area=100;
+function [cell_radius]=calculate_radius(no_of_cells,city_area)
 
 cell_area=city_area/no_of_cells;
 cell_radius=sqrt(cell_area/(1.5*sqrt(3)));
