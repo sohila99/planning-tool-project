485a68742f55d3f0a166774ef5fd10204d16c073
Sohila Noureldin <53787050+sohila99@users.noreply.github.com>
2022-05-16 13:19:14 +0200

    Update cluster_size.m

--- cluster_size.m
+++ cluster_size.m
@@ -1,5 +1,5 @@
 %part A.1
-
+%cluster size N
 %generate an array B of frequency reuse factor possible values
 X=zeros(1,100);
 c=0;
@@ -12,6 +12,11 @@ for j = 0:10
     end
 end
 
-%calculate cluster size N from the given SIR(dB)
-N_more_or_eq=(1/3)*(i*10^(SIRmin/10))^(2/n); 
-N = B( find ( B > N_more_or_eq, 1));
+%calculate cluster size N from the given SIR(dB) and according to the number of interfering channels i
+%There are two models for calculating N
+%Rappaport Model
+N_more_or_eq=(1/3)*(i*10^(SIRmin/10))^(2/n);  
+%Molisch Model
+%N_more_or_eq=(1/3)*((i*10^(SIRmin/10))^(1/n)+1)^2;
+N = B( find ( B >= N_more_or_eq, 1));
+N
